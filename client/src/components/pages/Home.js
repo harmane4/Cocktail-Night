8f717b30fc1a393c21602751a83f725f60dca87f
Elise Harman <elise_harman@hotmail.com>
2021-08-06 23:04:55 +0800

    Basic styling of landing page

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,25 @@
 import React from "react";
+import "./Home.css";
+import cocktail1 from "../../assets/images/cocktail111.jpg";
+import cocktail2 from "../../assets/images/cocktail222.jpg";
+import cocktail3 from "../../assets/images/cocktail333.jpg";
 
 export default function Home() {
   return (
     <div>
-      <h1>Cocktail Night</h1>
+      <div className="column">
+        <img className="img" src={cocktail2} alt="cocktail"></img>
+      </div>
+      <div className="column">
+        <img className="img" src={cocktail1} alt="cocktail"></img>
+      </div>
+
+      <div className="column">
+        <img className="img" src={cocktail3} alt="cocktail"></img>
+      </div>
+
+      <h1 className="heroHeading">#COCKTAIL NIGHT</h1>
+      <p className="heroText">Lorem epsum epsum epsum epsum epsum epsum</p>
     </div>
   );
 }

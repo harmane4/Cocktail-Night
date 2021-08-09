0f5dd0503a0779b1e58a011c6b1774aa407b55d9
Elise Harman <elise_harman@hotmail.com>
2021-08-09 22:25:05 +0800

    Random cocktail generated. Basic styling can be improved & oz converted to ml

--- client/src/components/pages/CocktailChoices.js
+++ client/src/components/pages/CocktailChoices.js
@@ -1,9 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./CocktailChoices.css";
+var convert = require("convert-units");
 
 export default function CocktailChoices() {
+  const [cocktails, setCocktail] = useState(null);
+  const [loading, setLoading] = useState(true);
+  console.log("cocktails", cocktails);
+
+  convert(1).from("fl-oz").to("ml");
+
+  useEffect(() => {
+    randomCocktail();
+
+    async function randomCocktail() {
+      const response = await fetch(
+        "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+      );
+      const data = await response.json();
+      const cocktailData = data.drinks[0];
+      console.log("cocktailData", cocktailData);
+
+      setCocktail(cocktailData);
+      setLoading(false);
+    }
+  }, []);
+
   return (
-    <div>
-      <h1>Cocktails</h1>
+    <div className="randomCocktailContainer">
+      <h1 className="heading">YOUR COCKTAIL</h1>
+      <h2>COCKTAIL NAME</h2>
+      {loading ? (
+        <div>...loading</div>
+      ) : (
+        <img className="image" src={cocktails.strDrinkThumb}></img>
+      )}
+      {loading ? <div>..loading</div> : <p>{cocktails.strDrink}</p>}
+      <h2>INGREDIENTS & MEASUREMENTS</h2>
+      {loading ? (
+        <div>..loading</div>
+      ) : (
+        <p>
+          {cocktails.strIngredient1} - {cocktails.strMeasure1}
+        </p>
+      )}
+      {loading ? (
+        <div>..loading</div>
+      ) : (
+        <p>
+          {cocktails.strIngredient2} - {cocktails.strMeasure2}
+        </p>
+      )}
+      {loading ? (
+        <div>..loading</div>
+      ) : (
+        <p>
+          {cocktails.strIngredient3} - {cocktails.strMeasure3}
+        </p>
+      )}
+      {loading ? (
+        <div>..loading</div>
+      ) : (
+        <p>
+          {cocktails.strIngredient4} - {cocktails.strMeasure4}
+        </p>
+      )}
+      <h2>METHOD</h2>
+      {loading ? <div>..loading</div> : <p>{cocktails.strInstructions}</p>}
     </div>
   );
+}
+
+{
+  /* <li>Cocktail Name: {cocktail.drinks[0].strDrink} </li>
+        <li>Ingredients: </li>
+        <li>Method:</li>
+        <li>Image:</li> */
 }

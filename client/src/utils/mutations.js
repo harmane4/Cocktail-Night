006b173a565af1733eee687f9884f323be266648
Elise Harman <elise_harman@hotmail.com>
2021-08-09 20:37:42 +0800

    Removing unnecessary comments

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -2,11 +2,10 @@ import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
   mutation loginUser($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
+    loginUser(email: $email, password: $password) {
       token
       user {
         _id
-        username
       }
     }
   }
